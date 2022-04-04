b5c23ea2e8b807e1ab5e956c045b3b8b07de4c64
Johann Erhard <johann.nachricht@gmail.com>
2022-04-04 21:09:33 +0100

    Add sensor & Add argument parser

--- example/start_api.py
+++ example/start_api.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 from sensor_api.web_wrapper import WebWrapper
 from sensor_api.auth_methods.web_none import start_app
@@ -7,9 +8,17 @@ import sensor_async.info as SENSOR_INFO
 
 
 def get_wrappers(sensors):
+    '''! Create web wrapper for each sensor with additional information and converters 
+    for the web api.
+
+    @param sensors List of configured sensors
+    @return wrappers List of sensor wrappers
+    '''
+
     wrappers = {}
 
     for sensor in sensors:
+        # USB webcam
         if str(sensor) == 'webcam':
             def stream_converter(data):
                 return b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + data + b'\r\n'
@@ -17,6 +26,7 @@ def get_wrappers(sensors):
             wrappers[str(sensor)] = WebWrapper(sensor, 'image/jpeg',
                                                'multipart/x-mixed-replace; boundary=frame', stream_converter)
 
+        # Temperature Sensor (One-Wire Interface as System Resource)
         if str(sensor) == 'temp':
             def stream_converter(data):
                 return f'data: {data} \n\n'
@@ -28,7 +38,15 @@ def get_wrappers(sensors):
 
 
 if __name__ == '__main__':
-    sensors = SENSOR_INFO.sensors_from_config('config.yaml')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', metavar='CONFIG', type=str, help='Sensor config file')
+    parser.add_argument('-p', metavar='PORT', type=int, help='Local webserver port')
+
+    args = parser.parse_args()
+
+    sensors = SENSOR_INFO.sensors_from_config(args.c)
     wrappers = get_wrappers(sensors)
 
-    start_app(5000, wrappers)
+    print(f'Configured sensors: {list(wrappers.keys())}')
+
+    start_app(args.p, wrappers)
